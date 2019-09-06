c4915ebdf93577ecb769fcb7a3ff9515efede649
Anderson Fernando Lopes Jacondino <anderson.jacondino@cwi.com.br>
2019-09-06 09:21:18 -0300

    table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+
+const columns = [{
+  dataField: 'id',
+  text: 'Product ID',
+  sort: true
+}, {
+  dataField: 'name',
+  text: 'Product Name',
+  sort: true,
+  filter: textFilter()
+}, {
+  dataField: 'price',
+  text: 'Product Price',
+  sort: true
+}];
+
+
+const products = [
+  {
+    id: 1,
+    name: 'product 1',
+    price: 100
+  },
+  {
+    id: 2,
+    name: 'product 2',
+    price: 200
+  },
+  {
+    id: 3,
+    name: 'product 3',
+    price: 300
+  },
+  {
+    id: 4,
+    name: 'product 4',
+    price: 400
+  },
+  {
+    id: 5,
+    name: 'product 5',
+    price: 500
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BootstrapTable bootstrap4 keyField='id' data={products} columns={columns} filter={ filterFactory() }/>
     </div>
   );
 }
